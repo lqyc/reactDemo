70be61e7015bb3d82e539aea55f72363d8a80cdd
覃予辰 <yuchen.qin@gemii.cc>
2018-03-06 15:14:46 +0800

    update

--- src/components/chart/BarChart.js
+++ src/components/chart/BarChart.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
+import 'echarts/lib/component/tooltip'
 
 var echarts = require('echarts/lib/echarts')
 require('echarts/lib/chart/bar')
